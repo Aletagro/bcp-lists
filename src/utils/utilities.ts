eab8f1413f2bf34e1440f147b9d13ea394a54196
Rukosuev Nikita <aletagro@yandex.ru>
2025-07-03 17:16:18 +0500

    fix types

--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -189,8 +189,12 @@ export const generateRoundPDF = async (
   document.body.removeChild(tableContainer)
 }
 
+type TransliterationMap = {
+  [key in 'а'|'б'|'в'|'г'|'д'|'е'|'ё'|'ж'|'з'|'и'|'й'|'к'|'л'|'м'|'н'|'о'|'п'|'р'|'с'|'т'|'у'|'ф'|'х'|'ц'|'ч'|'ш'|'щ'|'ъ'|'ы'|'ь'|'э'|'ю'|'я'|'А'|'Б'|'В'|'Г'|'Д'|'Е'|'Ё'|'Ж'|'З'|'И'|'Й'|'К'|'Л'|'М'|'Н'|'О'|'П'|'Р'|'С'|'Т'|'У'|'Ф'|'Х'|'Ц'|'Ч'|'Ш'|'Щ'|'Ъ'|'Ы'|'Ь'|'Э'|'Ю'|'Я']: string
+}
+
 const transliterate = (text: string) => {
-  const map = {
+  const map: TransliterationMap = {
     // Строчные буквы
     'а': 'a',    'б': 'b',    'в': 'v',    'г': 'g',    'д': 'd',
     'е': 'e',    'ё': 'e',    'ж': 'zh',   'з': 'z',    'и': 'i',
@@ -209,5 +213,5 @@ const transliterate = (text: string) => {
     'Э': 'E',    'Ю': 'Yu',   'Я': 'Ya'
   };
   return text
-    .replace(/[а-яА-ЯёЁ]/g, (char: string) => map[char] || char)
+    .replace(/[а-яА-ЯёЁ]/g, (char: string) => map[char as keyof TransliterationMap] || char)
 }
